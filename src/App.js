f24f1ad67e745ad8c0145ca76e25a90eedf47f9a
Sparkaz303 <sparkazofficial@gmail.com>
2023-11-02 19:31:00 -0400

    Assignment-6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Card from './Card';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>CS 230L</h1>
       <h2>Section - XX</h2>
       <p>WVU ID: XXXXXXX</p>
       <p>Hi I am X</p>
+      <Card/>
     </div>
   );
 }
